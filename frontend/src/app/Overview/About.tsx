19daf2263a0d43986320957b0f7a323a72d7389f
Rafaela Soares <rsoaresd@redhat.com>
2023-02-23 10:20:22 +0000

    adding documentation card

--- frontend/src/app/Overview/About.tsx
+++ frontend/src/app/Overview/About.tsx
@@ -8,7 +8,10 @@ import {
     DescriptionListDescription,
     DescriptionListGroup,
     DescriptionListTerm,
-    Gallery,
+    Grid,
+    GridItem,
+    Stack,
+    StackItem,
     Title,
 } from '@patternfly/react-core';
 import { getVersion } from '@app/utils/APIService';
@@ -43,82 +46,102 @@ export const About = () => {
 
 
     return (
-        <Gallery hasGutter style={{ display: "flex" }}>
-            <Card isRounded isCompact style={{ width: "65%", padding: "5px" }}>
-                <CardTitle>
-                    <Title headingLevel="h1" size="xl">
-                        About
-                    </Title>
-                </CardTitle>
-                <CardBody style={{ paddingLeft: '16px' }}>
-                    <Card isPlain isCompact >
-                        <CardTitle>What Is Quality Studio?</CardTitle>
-                        <CardBody>
-                            Quality Studio tries to unify in a single Dashboard all quality information about your product; from your commits to the costumers feedback.
-                        </CardBody>
-                    </Card>
-                    <Card isPlain isCompact >
-                        <CardTitle>Connectors for everything</CardTitle>
-                        <CardBody>Onboard team for your product and start to explore Quality Studio <b>Connectors</b>.</CardBody>
-                    </Card>
-                    <Card isPlain isCompact>
-                    <CardHeader><b>Jira Connector</b></CardHeader>
-                        <CardBody>
-                            Measure your bugs resolution times or retrieve information about what bugs are blocking your tests.
-                        </CardBody>
-                    </Card>
-                    <Card isPlain isCompact>
-                        <CardHeader><b>Openshift CI Connector</b></CardHeader>
-                        <CardBody>
-                            Connect with openshift CI and track information about presubmit, periodic or postsubmit jobs. Quality Studio offer a complete set of metrics about the impact of the tests execution
-                            in your repositories. Note that are only present the repositories with OpenShift CI prow jobs, as well as the job types available for each repository.
-                        </CardBody>
-                    </Card>
-                    <Card isPlain isCompact>
-                        <CardHeader><b>GitHub Connector</b></CardHeader>
-                        <CardBody>
-                            Observe detailed information from GitHub like GitHub Workflows, Pull Requests, Contributors.
-                        </CardBody>
-                    </Card>
-                    <Card isPlain isCompact>
-                        <CardHeader><b>Coverage Connector</b></CardHeader>
-                        <CardBody>
-                            Connect to <a href=""><b>CodeCov</b></a> and get the total coverage of your repository.
-                        </CardBody>
-                    </Card>
-                </CardBody>
-            </Card>
-            <Card isRounded style={{ width: "35%" }}>
-                <CardTitle>
-                    <Title headingLevel="h1" size="xl">
-                        Red Hat Quality Studio Details
-                    </Title>
-                </CardTitle>
-                <CardBody>
-                    <DescriptionList>
-                        <DescriptionListGroup>
-                            <DescriptionListTerm>Quality Studio version</DescriptionListTerm>
-                            <DescriptionListDescription>
-                                <span>{dashboardVersion}</span>
-                            </DescriptionListDescription>
-                        </DescriptionListGroup>
-                        <DescriptionListGroup>
-                            <DescriptionListTerm>Server API Status</DescriptionListTerm>
-                            <DescriptionListDescription>
-                                {serverAvailable && <span style={{ color: "darkgreen", verticalAlign: "middle", lineHeight: "2em", fontWeight: 500 }}> <CheckCircleIcon size={'sm'} ></CheckCircleIcon> OK </span>}
-                                {!serverAvailable && <span style={{ color: "darkred", verticalAlign: "middle", lineHeight: "2em", fontWeight: 500 }}> <ExclamationCircleIcon size={'sm'} ></ExclamationCircleIcon> DOWN </span>}
-                            </DescriptionListDescription>
-                        </DescriptionListGroup>
-                        <DescriptionListGroup>
-                            <DescriptionListTerm>Database Status</DescriptionListTerm>
-                            <DescriptionListDescription>
-                                {serverAvailable && <span style={{ color: "darkgreen", verticalAlign: "middle", lineHeight: "2em", fontWeight: 500 }}> <CheckCircleIcon size={'sm'} ></CheckCircleIcon> OK </span>}
-                                {!serverAvailable && <span style={{ color: "darkred", verticalAlign: "middle", lineHeight: "2em", fontWeight: 500 }}> <ExclamationCircleIcon size={'sm'} ></ExclamationCircleIcon> DOWN </span>}
-                            </DescriptionListDescription>
-                        </DescriptionListGroup>
-                    </DescriptionList>
-                </CardBody>
-            </Card>
-        </Gallery>
+        <Grid hasGutter>
+            <GridItem span={8}>
+                <Card isLarge>
+                    <CardTitle>
+                        <Title headingLevel="h1" size="xl">
+                            About
+                        </Title>
+                    </CardTitle>
+                    <CardBody style={{ paddingLeft: '16px' }}>
+                        <Card isPlain isCompact >
+                            <CardTitle>What Is Quality Studio?</CardTitle>
+                            <CardBody>
+                                Quality Studio tries to unify in a single Dashboard all quality information about your product; from your commits to the costumers feedback.
+                            </CardBody>
+                        </Card>
+                        <Card isPlain isCompact >
+                            <CardTitle>Connectors for everything</CardTitle>
+                            <CardBody>Onboard team for your product and start to explore Quality Studio <b>Connectors</b>.</CardBody>
+                        </Card>
+                        <Card isPlain isCompact>
+                            <CardHeader><b>Jira Connector</b></CardHeader>
+                            <CardBody>
+                                Measure your bugs resolution times or retrieve information about what bugs are blocking your tests.
+                            </CardBody>
+                        </Card>
+                        <Card isPlain isCompact>
+                            <CardHeader><b>Openshift CI Connector</b></CardHeader>
+                            <CardBody>
+                                Connect with openshift CI and track information about presubmit, periodic or postsubmit jobs. Quality Studio offer a complete set of metrics about the impact of the tests execution
+                                in your repositories. Note that are only present the repositories with OpenShift CI prow jobs, as well as the job types available for each repository.
+                            </CardBody>
+                        </Card>
+                        <Card isPlain isCompact>
+                            <CardHeader><b>GitHub Connector</b></CardHeader>
+                            <CardBody>
+                                Observe detailed information from GitHub like GitHub Workflows, Pull Requests, Contributors.
+                            </CardBody>
+                        </Card>
+                        <Card isPlain isCompact>
+                            <CardHeader><b>Coverage Connector</b></CardHeader>
+                            <CardBody>
+                                Connect to <a href=""><b>CodeCov</b></a> and get the total coverage of your repository.
+                            </CardBody>
+                        </Card>
+                    </CardBody>
+                </Card>
+            </GridItem>
+            <GridItem span={4}>
+                <Stack hasGutter>
+                    <StackItem>
+                        <Card isLarge>
+                            <CardTitle>
+                                <Title headingLevel="h1" size="xl">
+                                    Red Hat Quality Studio Details
+                                </Title>
+                            </CardTitle>
+                            <CardBody>
+                                <DescriptionList>
+                                    <DescriptionListGroup>
+                                        <DescriptionListTerm>Quality Studio version</DescriptionListTerm>
+                                        <DescriptionListDescription>
+                                            <span>{dashboardVersion}</span>
+                                        </DescriptionListDescription>
+                                    </DescriptionListGroup>
+                                    <DescriptionListGroup>
+                                        <DescriptionListTerm>Server API Status</DescriptionListTerm>
+                                        <DescriptionListDescription>
+                                            {serverAvailable && <span style={{ color: "darkgreen", verticalAlign: "middle", lineHeight: "2em", fontWeight: 500 }}> <CheckCircleIcon size={'sm'} ></CheckCircleIcon> OK </span>}
+                                            {!serverAvailable && <span style={{ color: "darkred", verticalAlign: "middle", lineHeight: "2em", fontWeight: 500 }}> <ExclamationCircleIcon size={'sm'} ></ExclamationCircleIcon> DOWN </span>}
+                                        </DescriptionListDescription>
+                                    </DescriptionListGroup>
+                                    <DescriptionListGroup>
+                                        <DescriptionListTerm>Database Status</DescriptionListTerm>
+                                        <DescriptionListDescription>
+                                            {serverAvailable && <span style={{ color: "darkgreen", verticalAlign: "middle", lineHeight: "2em", fontWeight: 500 }}> <CheckCircleIcon size={'sm'} ></CheckCircleIcon> OK </span>}
+                                            {!serverAvailable && <span style={{ color: "darkred", verticalAlign: "middle", lineHeight: "2em", fontWeight: 500 }}> <ExclamationCircleIcon size={'sm'} ></ExclamationCircleIcon> DOWN </span>}
+                                        </DescriptionListDescription>
+                                    </DescriptionListGroup>
+                                </DescriptionList>
+                            </CardBody>
+                        </Card>
+                    </StackItem>
+                    <StackItem>
+                        <Card isLarge>
+                            <CardTitle>
+                                <Title headingLevel="h1" size="xl">
+                                    Documentation
+                                </Title>
+                            </CardTitle>
+                            <CardBody>
+                                Find our <a href="https://github.com/redhat-appstudio/quality-dashboard" target="_blank" rel="noreferrer"><b>code at GitHub</b></a>
+                            </CardBody>
+                        </Card>
+                    </StackItem>
+                </Stack>
+            </GridItem>
+        </Grid>
     )
 };
