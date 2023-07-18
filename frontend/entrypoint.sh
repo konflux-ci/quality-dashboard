52ac1d7a1863eec18c1557fdf9296dd363f4927c
Flavius Lacatusu <flacatus@redhat.com>
2023-07-18 14:39:42 +0200

    Add replacements

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
 set -ex
 
-yarn start:dev
+REACT_APP_API_SERVER_URL_REPLACE=$(grep REACT_APP_API_SERVER_URL .env | cut -d '=' -f 2)
+DEX_ISSUER_REPLACE=$(grep DEX_ISSUER .env | cut -d '=' -f 2)
+FRONTEND_REDIRECT_URI_REPLACE=$(grep FRONTEND_REDIRECT_URI .env | cut -d '=' -f 2)
+DEX_APPLICATION_ID_REPLACE=$(grep FRONTEND_REDIRECT_URI .env | cut -d '=' -f 2)
+
+sed -i -- "s#$REACT_APP_API_SERVER_URL_REPLACE#$REACT_APP_API_SERVER_URL#g" "dist/main.bundle.js"
+sed -i -- "s#$REACT_APP_API_SERVER_URL_REPLACE#$REACT_APP_API_SERVER_URL#g" "dist/main.bundle.js.map"
+
+sed -i -- "s#$DEX_ISSUER_REPLACE#$DEX_ISSUER#g" "dist/main.bundle.js"
+sed -i -- "s#$DEX_ISSUER_REPLACE#$DEX_ISSUER#g" "dist/main.bundle.js.map"
+
+sed -i -- "s#$FRONTEND_REDIRECT_URI_REPLACE#$FRONTEND_REDIRECT_URI#g" "dist/main.bundle.js"
+sed -i -- "s#$FRONTEND_REDIRECT_URI_REPLACE#$FRONTEND_REDIRECT_URI#g" "dist/main.bundle.js.map"
+
+sed -i -- "s#$DEX_APPLICATION_ID_REPLACE#$DEX_APPLICATION_ID#g" "dist/main.bundle.js"
+sed -i -- "s#$DEX_APPLICATION_ID_REPLACE#$DEX_APPLICATION_ID#g" "dist/main.bundle.js.map"
+
+yarn start
