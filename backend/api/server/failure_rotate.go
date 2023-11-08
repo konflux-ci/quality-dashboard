a56ca91ba81d198fb9c1a74e6a1bee587604afad
Rafaela Soares <rsoaresd@redhat.com>
2023-11-08 16:40:06 +0000

    comment removal of closed issue from impact table

--- backend/api/server/failure_rotate.go
+++ backend/api/server/failure_rotate.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	failureV1Alpha1 "github.com/redhat-appstudio/quality-studio/api/apis/failure/v1alpha1"
-	"go.uber.org/zap"
 )
 
 func (s *Server) UpdateFailuresByTeam() {
@@ -17,14 +16,14 @@ func (s *Server) UpdateFailuresByTeam() {
 				s.cfg.Logger.Sugar().Warnf("Failed to get jira status:", err)
 			}
 
-			if jiraStatus == "Closed" {
-				err = s.cfg.Storage.DeleteFailure(team.ID, failure.ID)
-				if err != nil {
-					s.cfg.Logger.Sugar().Warnf("Failed to delete failure:", err)
-				} else {
-					s.cfg.Logger.Info("Deleted issue from impact table", zap.String("Jira Key", failure.JiraKey))
-				}
-			}
+			// if jiraStatus == "Closed" {
+			// 	err = s.cfg.Storage.DeleteFailure(team.ID, failure.ID)
+			// 	if err != nil {
+			// 		s.cfg.Logger.Sugar().Warnf("Failed to delete failure:", err)
+			// 	} else {
+			// 		s.cfg.Logger.Info("Deleted closed issue from impact table", zap.String("Jira Key", failure.JiraKey))
+			// 	}
+			// }
 
 			err = s.cfg.Storage.CreateFailure(failureV1Alpha1.Failure{
 				JiraKey:      failure.JiraKey,
