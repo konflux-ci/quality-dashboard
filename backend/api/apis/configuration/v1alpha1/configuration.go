a5fd2cbf17536ab39df3c967c62a5c3f494fc5d7
Rafaela Soares <rsoaresd@redhat.com>
2024-06-27 13:35:53 +0100

    add comments

--- backend/api/apis/configuration/v1alpha1/configuration.go
+++ backend/api/apis/configuration/v1alpha1/configuration.go
@@ -4,6 +4,7 @@ import (
 	"github.com/google/uuid"
 )
 
+// Configuration holds all the configuration related to team specifications.
 type Configuration struct {
 	ID            uuid.UUID `json:"id"`
 	TeamName      string    `json:"team_name"`
@@ -11,8 +12,12 @@ type Configuration struct {
 	BugSLOsConfig string    `json:"bug_slos_config"`
 }
 
+// JiraConfig holds the configuration for the Jira plugin.
 type JiraConfig struct {
-	BugsCollectQuery string   `json:"bugs_collect_query"`
-	CiImpactQuery    string   `json:"ci_impact_query"`
-	CiImpactBugs     []string `json:"ci_impact_bugs"`
+	// JQL query to save all the bugs for a specific team.
+	BugsCollectQuery string `json:"bugs_collect_query"`
+	// JQL query to grab all the bugs that are affecting CI.
+	CiImpactQuery string `json:"ci_impact_query"`
+	// List of Jira keys related to the bugs impacting CI based on the CiImpactQuery.
+	CiImpactBugs []string `json:"ci_impact_bugs"`
 }
