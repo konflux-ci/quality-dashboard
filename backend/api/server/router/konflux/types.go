b828ace04f0df7e295f99b1dcc1ac293f69e028a
Tomas Nevrlka <tnevrlka@redhat.com>
2024-07-22 17:01:59 +0200

    chore: KonfluxMetadata struct comments

--- backend/api/server/router/konflux/types.go
+++ backend/api/server/router/konflux/types.go
@@ -1,15 +1,35 @@
 package konflux
 
-import "time"
+import (
+	"time"
+)
 
+// KonfluxMetadata Represents metadata of tests used in Konflux metrics
 type KonfluxMetadata struct {
-	State           string    `json:"state"`
-	JobId           string    `json:"job_id"`
-	CreatedAt       time.Time `json:"created_at"`
-	JobType         string    `json:"job_type"`
-	JobName         string    `json:"job_name"`
-	JobUrl          string    `json:"job_url"`
-	ExternalImpact  bool      `json:"external_impact"`
-	RepositoryName  string    `json:"repository_name"`
-	GitOrganization string    `json:"git_organization"`
+	// State indicate if a job is running, failed, aborted etc
+	State string `json:"state"`
+
+	// Unique ID generated for every prow job.
+	JobId string `json:"job_id"`
+
+	// Date when a job was created in Openshift ci
+	CreatedAt time.Time `json:"created_at"`
+
+	// PostSubmits, periodics or presubmits
+	JobType string `json:"job_type"`
+
+	// Indicate the name of the job
+	JobName string `json:"job_name"`
+
+	// Url to some prow cluster
+	JobUrl string `json:"job_url"`
+
+	// Indicate if job was impacted by an external service
+	ExternalImpact bool `json:"external_impact"`
+
+	// Name of the repository where job was ran
+	RepositoryName string `json:"repository_name"`
+
+	// Name of the git organization where job was ran
+	GitOrganization string `json:"git_organization"`
 }
